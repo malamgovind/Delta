04be54c8bccfeb5d3eb236a99af38f4fcb0c9631
Malam Govind <malamgovindd1@gmail.com>
2023-12-30 21:28:32 +0530

    ok

---
+++ Day-15/javascript/04_conditional_statement.js
@@ -0,0 +1,13 @@
+console.log("//////////---if statements---///////////");
+console.log("*****statement false hoy to print na thay*****");
+console.log("before my if statement:");
+let age = 20;
+console.log(age);
+if (age >= 18) {
+    console.log("your statement true:");
+}
+if (age <= 18) {
+    console.log("your statement false:");
+}
+console.log("after my if statement:");
+console.log("----------------------------------------");
